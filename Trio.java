982a516935abfe82848feec6e25007cab98da5bd
jhJohnson16 <jacob.johnson4@stu.daviess.kyschools.us>
2025-04-07 14:30:17 +0000

    I tried

--- Trio.java
+++ Trio.java
@@ -1,1 +1,17 @@
-//Write your answer here
+public class Trio implements menuItem{
+    Sandwich sandwich;
+    Salad salad;
+    Drink drink;
+
+    public Trio(Sandwich sandwich, Salad salad, Drink drink){
+        sandwich = sandwich;
+        salad = salad;
+        drink = drink;
+    }
+
+    private String getName(){
+        return sandwich.getName() && salad.getName() && drink.getName();
+    }
+
+
+}
